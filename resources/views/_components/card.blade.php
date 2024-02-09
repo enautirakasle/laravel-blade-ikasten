6778963db21762635c99caeba3a2c7ed2c8c396c
Eñaut Agirre Arrizabalaga <enaut.irakasle@gmail.com>
2024-02-09 19:28:02 +0100

    slots eta yields

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div style="display: inline-block; border: 1px solid black; border-radius:5px; padding:10px; margin: 10px">
     <h3>{{ $title }}</h3>
     <p>{{ $content }}</p>
 </div>
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
     <h1>Services</h1>
+    @component('_components.card')
+        @slot('title', 'Service 1')
+        @slot('content', ' Lorem ipsum dolor set ...')
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Service 2')
+        @slot('content', ' Lorem ipsum dolor set ...')
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Service 3')
+        @slot('content', ' Lorem ipsum dolor set ...')
+    @endcomponent
+   
 @endsection
